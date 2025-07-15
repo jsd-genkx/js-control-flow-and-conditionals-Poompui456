1fd86da34a7ad07d1cd906d492b3d4f74effd157
Siriporn Worranawin <cheenoi30@gmail.com>
2025-07-15 11:55:57 +0700

    modified task.js: added answers

--- task02.js
+++ task02.js
@@ -5,3 +5,38 @@
 
 const userInput = "42";
 // Try changing to "hello", 10, null, NaN
+
+if (isNaN (userInput)) {
+  console.log("Not a number");
+} else {
+  console.log("It is a number");
+} 
+// it's a number.
+if (typeof userInput === "number") {
+  console.log("It is a number");
+} else {
+  console.log("Not a number");
+}
+// not a number.
+
+if (typeof userInput === "number" && !isNaN(userInput)) {
+  console.log("It is a number.");
+} else {
+  console.log("NOT a number.");
+}
+// NOT a number.
+
+const userInput = "hello";
+if (isNaN(userInput)) {
+  console.log("Not a number");
+} else {
+  console.log("It's a number");
+}
+// not a number.
+if (typeof userInput === "number") {
+  console.log("It's a number");
+} else {
+  console.log("Not a number");
+}
+// Not a number.
+
